859b8e61545bc22508e6762aac0cd6f461faef19
Samurai214567 <150684437+Samurai214567@users.noreply.github.com>
2025-09-26 19:17:57 +0300

    Create хеш_таблицы.java

---
+++ хеш_таблицы.java
@@ -0,0 +1,9 @@
+import java.util.HashMap;
+
+// Через HashMap
+HashMap<String, Integer> hashTable = new HashMap<>();
+hashTable.put("key1", 100);
+hashTable.put("key2", 200);
+System.out.println(hashTable.get("key1")); // 100
+
+// Собственная реализация через массив LinkedList
